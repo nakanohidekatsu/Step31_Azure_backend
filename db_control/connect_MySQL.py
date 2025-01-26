d01adb5b7f2f512cebb0b53afc07e59f3c1bd55e
nakanohidekatsu <hidekatsu.nakano@gmail.com>
2025-01-26 17:08:18 +0900

    Add files via upload

---
+++ db_control/connect_MySQL.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+
+import os
+from dotenv import load_dotenv
+
+# 環境変数の読み込み
+load_dotenv()
+
+# データベース接続情報
+DB_USER = os.getenv('MYSQL_USER')
+DB_PASSWORD = os.getenv('MYSQL_PASSWORD')
+DB_HOST = os.getenv('MYSQL_SERVER')
+DB_PORT = os.getenv('MYSQL_DB_PORT')
+DB_NAME = os.getenv('MYSQL_DB')
+
+# MySQLのURL構築
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+print("nakano DATABASE_URL:",DATABASE_URL)
+
+# エンジンの作成
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=3600
+)
